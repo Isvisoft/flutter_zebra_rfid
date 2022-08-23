40d0395dbb886be6893556f684084bdb3bf9d722
Ismael Monje <imonje@isvisoft.com>
2022-08-23 13:07:40 +0200

    first commit

---
+++ android/src/main/kotlin/com/isvisoft/flutter_zebra_rfid/FlutterZebraRfidPlugin.kt
@@ -0,0 +1,55 @@
+package com.isvisoft.flutter_zebra_rfid
+
+import android.util.Log
+import androidx.annotation.NonNull
+import com.zebra.rfid.api3.TagData
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+/** FlutterZebraRfidPlugin */
+class FlutterZebraRfidPlugin : FlutterPlugin, MethodCallHandler,
+  RFIDHandler.ResponseHandlerInterface {
+
+  private lateinit var channel: MethodChannel
+  private lateinit var rfidHandler: RFIDHandler
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_zebra_rfid")
+    channel.setMethodCallHandler(this)
+
+    rfidHandler = RFIDHandler()
+    rfidHandler.onCreate(this, flutterPluginBinding.applicationContext)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun handleTagdata(tagData: Array<TagData>) {
+    val sb = StringBuilder()
+    for (index in tagData.indices) {
+      sb.append(tagData[index].tagID.trimIndent())
+    }
+
+    Log.d("8888", sb.toString())
+  }
+
+  override fun handleTriggerPress(pressed: Boolean) {
+    if (pressed) {
+      rfidHandler.performInventory()
+    } else {
+      rfidHandler.stopInventory()
+    }
+  }
+}
---
+++ example/android/app/src/main/kotlin/com/isvisoft/flutter_zebra_rfid_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.isvisoft.flutter_zebra_rfid_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
